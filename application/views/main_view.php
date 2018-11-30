17c7667d39fc4a922a4beef7813c09441d6b67bd
ViktorVinokurov <45365318+ViktorVinokurov@users.noreply.github.com>
2018-11-30 15:09:18 +0400

    Update main_view.php

--- application/views/main_view.php
+++ application/views/main_view.php
@@ -1,22 +1,44 @@
+
 <div class="container">
 <div class="row">
 <div class="col-lg-3 col-md-3"></div>
 <div class="col-lg-6 col-md-6 col-sm-12 col-xl-12"></div>
-    <div class="container">
-        <div class="row controls">
-            <div class="col-md-12">
-                <h2>ToDo list</h2>
-                <div class="form-group">
-                    <label>Что будете делать?</label>
-                    <input type="text" class="form-control" id="todo-input" placeholder="Введите задачу">
-                </div>
-                <button type="button" class="btn btn-default">Очистить список</button>
-            </div>
+</br>
+<!--Добавлям задачу-->
+
+        <div class="form-group">
+            <label class="sr-only">Добавить задачу</label>
+            <input type="text" class="form-control form-control-lg" name="insertlist" id="actlist" placeholder="Добавить задачу"/>
         </div>
-        <div class="row">
-            <ol class="todo-list"></ol>
-        </div>
-    </div>
+        <input type="submit" name="ins" id="ins" class="btn btn-primary" value="Добавить"/>
+
+
+   <h3 class="text-center">Лист задач</h3>
+    <!--Выводим задачи-->
+    <table class="table text-center table-hover">
+        <thead>
+            <tr>
+                <td>№</td>
+                <td>Задание</td>
+                <td>Действие</td>
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+            foreach($data as $row)
+            {
+                echo '<tr>
+                        <td>'.$row['ID'].'</td>
+                        <td>'.$row['Name'].'</td>
+                        <td>
+                            <a class="btn btn-primary" href="?red_id='.$row['ID'].'">Изменить</a> 
+                            <a class="btn btn-danger" href="main?del_id='.$row['ID'].'">Удалить</a>
+                        </td>
+                      </tr>';
+            }
+            ?>
+        </tbody>
+    </table>
 <div class="col-lg-3 col-md-3"></div>
 </div>
 </div>

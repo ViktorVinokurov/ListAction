2bfc8256a9bdfaa903c53ed4e3cf77f0aa045534
ViktorVinokurov <45365318+ViktorVinokurov@users.noreply.github.com>
2018-11-30 15:03:13 +0400

    Update controller_main.php

--- application/controllers/controller_main.php
+++ application/controllers/controller_main.php
@@ -1,11 +1,19 @@
 <?php
 class Controller_Main extends Controller
 {
-    /**
-     * Controller_Main constructor.
-     */
-   public function action_index()
-   {
-       $this->view->generate('main_view.php','template_view.php');
-   }
+    function __construct()
+    {
+        $this->model = new Model_main();
+        $this->view = new View();
+    }
+
+
+    public function action_index()
+    {
+        $data = $this->model->get_data();
+        $data1 = $this->model->addlisting($_POST["insertlist"]);
+        $data2 = $this->model->removeaction($_GET['del_id']);
+        $this->view->generate('main_view.php','template_view.php', $data, $data1, $data2);
+    }
+
 }

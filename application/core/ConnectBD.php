1f7fa0558be2231126f0a8ea130754810a939174
ViktorVinokurov <45365318+ViktorVinokurov@users.noreply.github.com>
2018-11-30 15:05:14 +0400

    Update ConnectBD.php

--- application/core/ConnectBD.php
+++ application/core/ConnectBD.php
@@ -1,15 +1,23 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Виктор
- * Date: 25.11.2018
- * Time: 19:02
- */
 
-namespace application\core;
+class ConnectBD {
+    private $engine;
+    private $host;
+    private $database;
+    private $user;
+    private $pass;
 
+    private static $_db = NULL;
 
-class ConnectBD
-{
+    private function __construct(){}
+    private function __sleep(){}
+    private function __wakeup(){}
+    private function __clone(){}
 
+    public static function DB()
+    {
+        if(NULL === self::$_db)
+            self::$_db = new PDO('mysql:host=localhost;dbname=quest', 'root', '');
+        return self::$_db;
+    }
 }
